b04a45237b8573d7eabe7c1f3fa94906d2ae2692
Samael <jeremie.joannard@epitech.eu>
2024-09-04 11:27:45 +0200

    adding ship upgrades
    [MOD] AlienSurvivor/Assets/Prefabs/ships/ship1.prefab, [MOD] AlienSurvivor/Assets/Prefabs/ships/ship2.prefab, [MOD] AlienSurvivor/Assets/Scenes/SampleScene.unity, [MOD] AlienSurvivor/Assets/Scripts/PlayerStats.cs, [MOD] AlienSurvivor/Assets/Scripts/Weapons.cs, [ADD] AlienSurvivor/Assets/Prefabs/ships/Ship1, [ADD] 1.prefab.meta, [ADD] AlienSurvivor/Assets/Prefabs/ships/Ship2, [ADD] 1.prefab.meta, [ADD] AlienSurvivor/Assets/Prefabs/ships/Ship3.prefab, [ADD] AlienSurvivor/Assets/Prefabs/ships/Ship3.prefab.meta,

--- AlienSurvivor/Assets/Scripts/PlayerStats.cs
+++ AlienSurvivor/Assets/Scripts/PlayerStats.cs
@@ -5,20 +5,87 @@ using UnityEngine;
 public class PlayerStats : MonoBehaviour
 {
     public int life = 100;
+    public int maxLife = 100;
     public int armor = 0;
-    public int damageMultiplier = 1;
-    public int speedMultiplier = 1;
+    public float damageMultiplier = 1;
+    public float speedMultiplier = 1;
+    public int level = 1;
+
+    public List<GameObject> shipsLevels = new List<GameObject>();
+    public GameObject ship;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        ship = Instantiate(shipsLevels[level - 1], new Vector3(transform.position.x, transform.position.y, transform.position.z - 1), transform.rotation);
+        ship.transform.parent = transform;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void upgradeLife(int amount)
+    {
+        life += amount;
+        maxLife += amount;
+    }
+
+    void heal(int amount)
+    {
+        life += amount;
+        if (life > maxLife)
+        {
+            life = maxLife;
+        }
+    }
+
+    void upgradeArmor(int amount)
+    {
+        armor += amount;
+    }
+
+    void upgradeDamageMultiplier(float amount)
+    {
+        damageMultiplier += amount;
+    }
+
+    void upgradeSpeedMultiplier(float amount)
+    {
+        speedMultiplier += amount;
+    }
+
+    void takeDamage(int amount)
+    {
+        if (armor > 0)
+        {
+            armor -= amount;
+            if (armor < 0)
+            {
+                life += armor;
+                armor = 0;
+            }
+        }
+        else
+        {
+            life -= amount;
+        }
+    }
+
+    void levelUp()
+    {
+        upgradeLife(10);
+        heal(maxLife);
+        upgradeArmor(5);
+        upgradeDamageMultiplier(0.5f);
+        upgradeSpeedMultiplier(0.5f);
+        level++;
+
+        Destroy(ship);
+        ship = Instantiate(shipsLevels[level - 1], new Vector3(transform.position.x, transform.position.y, transform.position.z - 1), transform.rotation);
+        ship.transform.parent = transform;
     }
 }
--- AlienSurvivor/Assets/Scripts/Weapons.cs
+++ AlienSurvivor/Assets/Scripts/Weapons.cs
@@ -12,15 +12,17 @@ public class Weapons : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        for (int i = 0; i < weapons.Count; i++)
-        {
-            weapons[i].GetComponent<Blaster>().player = player;
-        }
+        weapons[0].GetComponent<Blaster>().player = player;
+        weapons[1].GetComponent<Scie>().player = player;
+        weapons[2].GetComponent<OrbitalStrike>().player = player;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        weapons[0].GetComponent<Blaster>().level = player.GetComponent<PlayerStats>().level;
+        weapons[1].GetComponent<Scie>().level = player.GetComponent<PlayerStats>().level;
+        weapons[2].GetComponent<OrbitalStrike>().level = player.GetComponent<PlayerStats>().level;
+        weapons[3].GetComponent<Shield>().level = player.GetComponent<PlayerStats>().level;
     }
 }
