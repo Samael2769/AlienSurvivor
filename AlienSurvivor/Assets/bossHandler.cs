de5e4f561417a7f46b61e8657f5ed5dda106bea6
Samael <jeremie.joannard@epitech.eu>
2024-09-09 04:51:18 +0200

    Adding boss and minion spawn
    [MOD] AlienSurvivor/Assets/Prefabs/EnemiesFinal/Enemy_Insect1.prefab, [MOD] AlienSurvivor/Assets/Prefabs/EnemiesFinal/Enemy_Ship1.prefab, [MOD] AlienSurvivor/Assets/Scenes/SampleScene.unity, [ADD] AlienSurvivor/Assets/Prefabs/EnemiesFinal/Boss1.prefab, [ADD] AlienSurvivor/Assets/Prefabs/EnemiesFinal/Boss1.prefab.meta, [ADD] AlienSurvivor/Assets/Prefabs/EnemiesFinal/Boss2.prefab, [ADD] AlienSurvivor/Assets/Prefabs/EnemiesFinal/Boss2.prefab.meta, [ADD] AlienSurvivor/Assets/Prefabs/EnemiesFinal/EnemyDemon1.prefab, [ADD] AlienSurvivor/Assets/Prefabs/EnemiesFinal/EnemyDemon1.prefab.meta, [ADD] AlienSurvivor/Assets/Prefabs/EnemiesFinal/EnemyProjectile.prefab, [ADD] AlienSurvivor/Assets/Prefabs/EnemiesFinal/EnemyProjectile.prefab.meta, [ADD] AlienSurvivor/Assets/Prefabs/EnemiesFinal/Minion1.prefab, [ADD] AlienSurvivor/Assets/Prefabs/EnemiesFinal/Minion1.prefab.meta, [ADD] AlienSurvivor/Assets/Prefabs/EnemiesFinal/Minion2.prefab, [ADD] AlienSurvivor/Assets/Prefabs/EnemiesFinal/Minion2.prefab.meta, [ADD] AlienSurvivor/Assets/bossHandler.cs, [ADD] AlienSurvivor/Assets/bossHandler.cs.meta,

---
+++ AlienSurvivor/Assets/bossHandler.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bossHandler : MonoBehaviour
+{
+    public GameObject boss;
+    public GameObject player;
+    public List<GameObject> minions = new List<GameObject>();
+
+    public int life = 100;
+    public int maxLife = 100;
+    public int level = 1;
+
+    public float spawnRate = 5.0f;
+    public float spawnTimer = 0.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        spawnTimer += Time.deltaTime;
+        if (spawnTimer >= spawnRate)
+        {
+            spawnTimer = 0.0f;
+            spawnMinion();
+        }
+    }
+
+    public void spawnMinion()
+    {
+        GameObject minion = Instantiate(minions[Random.Range(0, minions.Count)], boss.transform.position, Quaternion.identity);
+        minion.GetComponent<EnemyHandler>().player = player;
+    }
+
+    public void takeDamage(int damage)
+    {
+        life -= damage;
+        if (life <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            collision.gameObject.GetComponent<PlayerStats>().takeDamage(10);
+        }
+        if (collision.gameObject.tag == "PlayerProjectile")
+        {
+            takeDamage(player.GetComponent<PlayerStats>().damage);
+            Destroy(collision.gameObject);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            collision.gameObject.GetComponent<PlayerStats>().takeDamage(10);
+        }
+        if (collision.gameObject.tag == "PlayerProjectile")
+        {
+            takeDamage(player.GetComponent<PlayerStats>().damage);
+            Destroy(collision.gameObject);
+        }
+    }
+}
